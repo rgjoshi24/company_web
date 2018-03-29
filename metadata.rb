8df9162c807ea2e7260e8abdafded63a27985bec
Ramachandra G Joshi <ramachandra@Ramachandras-MacBook-Pro.local>
2018-03-29 16:00:55 -0400

    Wrapper company_web created

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,8 @@ description 'Installs/Configures company_web'
 long_description 'Installs/Configures company_web'
 version '0.1.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
+depends 'myiis'
+depends 'apache'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
